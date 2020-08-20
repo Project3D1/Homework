d5c9442be42af643baa9c844568d18a6fa7c7e0d
Ivan <project3d@my.com>
2020-08-21 01:37:11 +0300

    homework1

---
+++ src/main/java/Homework1/exercise6.java
@@ -0,0 +1,15 @@
+public class exercise6 {
+    public static boolean task6(int a) {
+
+        if (a < 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(task6(-5));
+    }
+}
