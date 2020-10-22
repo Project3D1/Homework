5a94c0639c82eff62fe96374d94d7670c9a50f0f
Ivan <Project3d@my.com>
2020-10-22 15:36:54 +0300

    Homework3_1

---
+++ src/main/java/Homework3_1/Cat.java
@@ -0,0 +1,7 @@
+package Homework3_1;
+
+public class Cat {
+    public void info() {
+        System.out.println("This is cat");
+    }
+}
---
+++ src/main/java/Homework3_1/Mas.java
@@ -0,0 +1,28 @@
+package Homework3_1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Mas<T> {
+    private T[] mas;
+
+    public Mas(T... mas) { this.mas = mas; }
+
+    public void masElementsExchange() {
+        T tmp = mas[0];
+        mas[0] = mas[1];
+        mas[1] = tmp;
+    }
+
+    public void sebObj(T[] mas) { this.mas = mas; }
+
+    public T[] getObj() { return mas; }
+
+    public void info() { for (T item : mas)
+        System.out.println(item.getClass().getName()); }
+
+    public ArrayList<T> arrayToArrayList() {
+        ArrayList<T> list = new ArrayList<>(Arrays.asList(mas));
+        return list;
+    }
+}
